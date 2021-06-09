de704d89b872308b73b3110bc7c392f12be7d305
sreerajpr06 <sreerajpr06@gmail.com>
2021-06-09 16:00:25 +0530

    Minor Fixes

--- models.py
+++ models.py
@@ -6,8 +6,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-database_uri = os.environ.get('DATABASE_URL')
-database_uri.replace("postgres", "postgresql")
+database_uri = os.environ.get('DATABASE_URL').replace("postgres", "postgresql")
 engine = create_engine(database_uri)
 db_session = scoped_session(sessionmaker(autocommit=False,
                                          autoflush=False,
